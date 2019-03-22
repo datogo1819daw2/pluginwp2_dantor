aaff17eb43357a5fd90c69a25ff94e94c5abaf10
root <root@daw2m08-datogo.fjeclot.net>
2019-03-22 15:38:42 +0100

    Commit 4

--- peupagina.php
+++ peupagina.php
@@ -1,3 +1,4 @@
+
 <?php
 /*
 Plugin Name: Peu de pàgina per defecte per a noves entrades 
@@ -19,9 +20,18 @@ function admin_menu_peu_pagina(){
 }
 
 add_action('admin_menu','admin_menu_peu_pagina');
+add_action('wp_footer','afegPeuPagDef');
+
+function afegPeuPagDef(){
+    echo "<div style='color: red;
+ 	   font-size: 10px;
+ 	   margin: 10px;'>".get_option('footer_text')."</div>";
+}
 
 function nouPeuPagDef(){
 	include_once('formPeuPag.php');
 }
+
  
 ?>
+
